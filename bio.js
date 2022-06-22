baf157bddb5448da1c011b10d4edeac20932cda8
serendipity04 <78849868+serendipity04@users.noreply.github.com>
2022-06-22 18:32:58 +0100

    first push

---
+++ bio.js
@@ -0,0 +1,9 @@
+// using the get element method
+document.getElementById('name').innerHTML = " Obinka Ugonwa ";
+document.getElementById('height').innerHTML = " 170cm";
+document.getElementById('country').innerHTML = " Nigeria ";
+
+// using the write method
+document.write("My name is Obinka Ugonwa, ")
+document.write("I measure 170cm in height, ")
+document.write("I'm a citizen and resident of Nigeria.")
